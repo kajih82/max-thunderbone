9b630f620586f8135cfdd06107efe41358d688eb
kajih <kajih82@gmail.com>
2025-12-22 22:30:08 -0500

    fixed airborne state transition problem when jumping in place.

--- assets/scripts/player/states/grounded_state.gd
+++ assets/scripts/player/states/grounded_state.gd
@@ -4,3 +4,8 @@ func _on_grounded_state_physics_processing(_delta: float) -> void:
 	if Input.is_action_just_pressed("jump") and player_controller.is_on_floor():
 		player_controller.jump()
 		player_controller.state_chart.send_event("onAirborne")
+	
+	# if the player is airborne without jumping change the state.
+	# happens if the player falls without jumping.
+	if not player_controller.is_on_floor():
+		player_controller.state_chart.send_event("onAirborne")
